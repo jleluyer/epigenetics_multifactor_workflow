bd576fd73819ac29976a6865974bc90c6e04650e
Jeremy LE LUYER <jleluyer@datarmor1.ib0.ice.ifremer.fr>
2018-01-12 19:01:37 +0000

    first commit

--- 00_scripts/04_methpipe.sh
+++ 00_scripts/04_methpipe.sh
@@ -17,13 +17,36 @@ GENOME="04_reference/GCF_002021735.1_Okis_V1_genomic.trim.fa"
 DATA_FOLDER="05_results"
 base=__BASE__
 
+#global variables
+GENOME="04_reference/genome_filtered.fa"
+DATA_FOLDER="05_results"
+DATAINPUT="03_trimmed"
+base=__BASE__
 
-LC_ALL=C sort -k 1,1 -k 2,2n -k 3,3n -k 6,6 -o "$DATA_FOLDER"/"$base".mr.sorted_start "$DATA_FOLDER"/"$base".mr
 
+LC_ALL=C sort -k 1,1 -k 2,2n -k 3,3n -k 6,6 -o "$DATA_FOLDER"/"$base".mr.sorted_start "$DATAINPUT"/"$base".mr
+
+rm "$DATAINPUT"/"$base".mr
+
+# Remove putative PCR duplicates
 duplicate-remover -S "$DATA_FOLDER"/"$base".dremove_stat.txt -o "$DATA_FOLDER"/"$base".mr.dremove "$DATA_FOLDER"/"$base".mr.sorted_start
 
-methcounts -cpg-only -c $GENOME -o "$DATA_FOLDER"/"$base".meth "$DATA_FOLDER"/"$base".mr.sorted_start
+rm "$DATA_FOLDER"/"$base".mr.sorted_start
 
+LC_ALL=C sort -k 1,1 -k 2,2n -k 3,3n -k 6,6 -o "$DATA_FOLDER"/"$base".mr.dremove.sort "$DATA_FOLDER"/"$base".mr.dremove
+
+rm "$DATA_FOLDER"/"$base".mr.dremove
+
+# Compute methylation level
+methcounts -cpg-only -c $GENOME -o "$DATA_FOLDER"/"$base".meth "$DATA_FOLDER"/"$base".mr.dremove.sort
+
+# Compute symmetric CpGs contexts
 symmetric-cpgs -o "$DATA_FOLDER"/"$base"_CpG.meth "$DATA_FOLDER"/"$base".meth
 
-bsrate -c $GENOME -o "$DATA_FOLDER"/"$base".bsrate "$DATA_FOLDER"/"$base".mr.dremove
+# Compute methylation stats
+levels -o "$DATA_FOLDER"/"$base".levels "$DATA_FOLDER"/"$base".meth
+
+# Compute conversion rate
+bsrate -c $GENOME -o "$DATA_FOLDER"/"$base".bsrate "$DATA_FOLDER"/"$base".mr.dremove.sort
+
+rm "$DATA_FOLDER"/"$base".mr.dremove.sort
