05e0256ae2ad891d45f4aeb43a7881339ebe5633
Jeremy LE LUYER <jleluyer@datarmor1.ib0.ice.ifremer.fr>
2018-01-12 18:29:00 +0000

    first commit

--- 00_scripts/utility_scripts/prepare_methylkit_input.sh
+++ 00_scripts/utility_scripts/prepare_methylkit_input.sh
@@ -12,7 +12,7 @@ DATA_FOLDER="05_results"
 base=__BASE__
 
 # prepare file for dss
-awk '{print $1"."$2,$1,"F",$2,$6}' "$DATA_FOLDER"/"$base"_CpG.meth >"$DATA_FOLDER"/"$base".methyl.1.temp
+awk '{print $1"."$2,$1,$2,"F",$6}' "$DATA_FOLDER"/"$base"_CpG.meth >"$DATA_FOLDER"/"$base".methyl.1.temp
 awk '{printf "%.2f\n",$5*100}' "$DATA_FOLDER"/"$base"_CpG.meth >"$DATA_FOLDER"/"$base".methyl.2.temp
 awk '{printf "%.2f\n",100-($5*100)}' "$DATA_FOLDER"/"$base"_CpG.meth >"$DATA_FOLDER"/"$base".methyl.3.temp
 paste "$DATA_FOLDER"/"$base".methyl.1.temp "$DATA_FOLDER"/"$base".methyl.2.temp "$DATA_FOLDER"/"$base".methyl.3.temp >"$DATA_FOLDER"/"$base".temp.methyl
