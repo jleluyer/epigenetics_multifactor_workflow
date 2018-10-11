79ca0003bb2ff35cbab21a2e6163b083677cc748
jeremy <jleluyer@ta-072.ifremer.fr>
2018-10-11 12:36:25 -1000

    trim_galore > fastp & iranges > bedtools

--- 00_scripts/01_trimming.sh
+++ 00_scripts/01_trimming.sh
@@ -2,7 +2,7 @@
 #PBS -N trimming.__BASE__
 #PBS -o 98_log_files/log-trimming.__BASE__.out
 #PBS -l walltime=20:00:00
-#PBS -l mem=50g
+#PBS -l mem=100g
 #PBS -r n
 
 cd $PBS_O_WORKDIR
@@ -16,13 +16,16 @@ LOG_FOLDER="98_log_files"
 cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 
 #variables
-LENGTH=100
-QUAL=20
-ERROR_RATE="0.2"
-OUTPUT="03_trimmed"
 base=__BASE__
+INPUT="02_data"
+OUTPUT="03_trimmed"
 
-trim_galore --illumina --fastqc -q $QUAL --gzip --trim1 --paired 02_data/"$base"_R1.fastq.gz 02_data/"$base"_R2.fastq.gz -o $OUTPUT
+
+fastp --thread 12 --compression 1 -i "$INPUT"/"$base"_R1.fastq.gz -I "$INPUT"/"$base"_R2.fastq.gz \
+	--length_required="$LENGTH" --qualified_quality_phred="$QUAL" \
+	--correction \
+	--trim_tail1=1 --trim_tail2=1 \
+	-o "$OUTPUT"/"$base"_R1.fastq.gz -O "$OUTPUT"/"$base"_R2.fastq.gz
 
 done 2>&1 | tee 98_log_files/"$TIMESTAMP"_trimmgalore_wgbs.log
 
